19c808e2fa334c7baebc6cd2e644bdeb398c8c84
aminzo <siyatmohammedcarab@gmail.com>
2025-09-28 21:47:32 +0300

    i did some changes

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let  num1 = 2; 
+let  num2 = 31;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = 14%5;
+
+
+let max =  Math.max (1,5,16,20);
